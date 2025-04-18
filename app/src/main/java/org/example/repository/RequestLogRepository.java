c00948b963de65fe9520dc84e5e749d5f1d6ab9d
Danil Zenin <zenindanil2000@gmail.com>
2025-04-18 13:13:24 +0300

    add repository

---
+++ app/src/main/java/org/example/repository/RequestLogRepository.java
@@ -0,0 +1,9 @@
+package org.example.repository;
+
+import org.example.model.RequestLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestLogRepository extends JpaRepository<RequestLog, Long> {
+}
