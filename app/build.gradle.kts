ad1ac496aa04eb95591595acaa3056996f1dd12d
Danil Zenin <zenindanil2000@gmail.com>
2025-04-18 13:14:16 +0300

    add project settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,15 +23,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.ws:spring-ws-core")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.springframework.security:spring-security-test:5.8.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.hamcrest:hamcrest:2.2")
     testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    testImplementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
+
 }
 
 tasks.test {
--- app/src/main/java/org/example/Application.java
+++ app/src/main/java/org/example/Application.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Application.class, args);
     }
 }
